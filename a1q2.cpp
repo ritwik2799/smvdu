f6b87cefe04303e28da78a24435c1e1b2833ddfb
ritwik2799 <56026471+ritwik2799@users.noreply.github.com>
2019-10-01 17:29:43 +0530

    More user friendly
    A more user friendly code.

--- a1q2.cpp
+++ a1q2.cpp
@@ -5,17 +5,19 @@ code description: checking if m is exact multiple of n. output will be 0 if m is
 int main()
 {
   int m, n, a;      //decleration of variables
+  printf("Enter any 2 no.s");
   scanf("%d", &m);  //taking the input of m
   scanf("%d", &n);  //taking the input of n
   
   if ( m%n == 0)    //logical operation for checking if m is a multiple of n
   {
     a = m/n;        // m divided by n stored in a variable
-    printf("%d",a); 
+    printf("%d is a exact multiple of %d and %d ",a,m,n); 
   }
   else              //if the logic holds false this else statement will be carried out  
   {
-    printf("0");
+    printf("Not an exact multiple");
   }
   return 0;
 }
+
